1a04ded041c273752a65ae25345e9551222ac5a5
Enrique Ávila Rodríguez <enrique.avila.y@gmail.com>
2020-02-06 11:24:32 +0100

    Replaced last remaining mentions of Manga Scan Helper with Miharu.

--- Miharu Scan Helper/Properties/Resources.Designer.cs
+++ Miharu Scan Helper/Properties/Resources.Designer.cs
@@ -38,7 +38,7 @@ namespace Miharu.Properties {
 		internal static global::System.Resources.ResourceManager ResourceManager {
 			get {
 				if ((resourceMan == null)) {
-					global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Manga_Scan_Helper.Properties.Resources", typeof(Resources).Assembly);
+					global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Miharu.Properties.Resources", typeof(Resources).Assembly);
 					resourceMan = temp;
 				}
 				return resourceMan;
