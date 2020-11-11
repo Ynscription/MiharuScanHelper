40d26a5415e759e87a7935ea16752c663780dc5a
Ynscription <enrique.avila.y@gmail.com>
2020-11-11 13:37:48 +0100

    Finished implementing Jisho view.

--- Miharu Scan Helper/FrontEnd/TextEntry/JishoDictEntryView.xaml.cs
+++ Miharu Scan Helper/FrontEnd/TextEntry/JishoDictEntryView.xaml.cs
@@ -1,7 +1,7 @@
 ﻿using Miharu.BackEnd.Data;
 using System;
-using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Documents;
 
 namespace Miharu.FrontEnd.TextEntry
 {
@@ -19,12 +19,50 @@ namespace Miharu.FrontEnd.TextEntry
 				_entry = value;
 				if (_entry != null) {
 					WordHyperText.Word = _entry.Item1;
-					DefinitionsTextBlock.Text = _entry.Item2;
+					ProcessDefinitions(_entry.Item2);
 				}
 			}
 		}
 		
-        
+        private void ProcessDefinitions (string def) {
+			DefinitionsTextBlock.Inlines.Clear();
+			bool first = true;
+			foreach (string line in def.Split(new string[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries)) {
+				char flag = line [0];
+				Run r;
+				switch (flag) {
+					case '$':
+						if (first)
+							first = false;
+						else
+							DefinitionsTextBlock.Inlines.Add(new LineBreak());
+						r = new Run();
+						r.Style = (System.Windows.Style)Resources["SmallAndGray"];
+						r.Text = line.Substring(1);
+						DefinitionsTextBlock.Inlines.Add(r);						
+						break;
+					case '#':
+						DefinitionsTextBlock.Inlines.Add(new LineBreak());
+						r = new Run();
+						r.Style = (System.Windows.Style)Resources["Gray"];
+						r.Text = line.Substring(1) + " ";
+						DefinitionsTextBlock.Inlines.Add(r);
+						break;
+					case '@':
+						r = new Run();
+						r.Text = line.Substring(1) + " ";
+						DefinitionsTextBlock.Inlines.Add(r);
+						break;
+					case '&':
+						r = new Run();
+						r.Style = (System.Windows.Style)Resources["SmallAndGray"];
+						r.Text = line.Substring(1) + " ";
+						DefinitionsTextBlock.Inlines.Add(r);	
+						break;
+				}
+
+			}
+		}
 
 			
 
